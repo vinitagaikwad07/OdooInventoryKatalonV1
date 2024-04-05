5dad0f943dddb7b2563c8f19fd219c777ba30d74
platonium1234 <platonium.automation@gmail.com>
2024-04-05 11:35:40 -0400

    Commit

---
+++ Test Suites/Goods Receipt.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Goods Receipt</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>484b14ff-7d54-49a5-b100-0e570a92d69f</testSuiteGuid>
+   <testCaseLink>
+      <guid>ea1caa92-4451-4f6d-a8ca-e53b63f9b443</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login to Odoo Inventory ERP System</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
